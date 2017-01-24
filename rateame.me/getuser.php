25c4ac53f979aec29041e7fcce91bda613d68db8
Gabriel <gabrielc007@gmail.com>
2017-01-24 09:01:06 -0500

    cleaned up php file

--- rateame.me/getuser.php
+++ rateame.me/getuser.php
@@ -3,11 +3,9 @@ $data = json_decode(stripslashes($_POST['data']));
 
 $dankness = 0;
 
- // echo 'executed' . PHP_EOL;
 
 $con = mysqli_connect('127.0.0.1','root','root','images', 3306);
 
-//2d214d83d51c0c7a5e220616bb0b10b4ab0afdf49d70cad0
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
@@ -19,7 +17,7 @@ $counter = 0;
 
  foreach($data as $d) {
     $sql="SELECT SUM(upvoteCount) FROM memes WHERE tag1 = '".$d."' OR tag2 = '".$d."' OR tag3 = '".$d."' OR tag4 = '".$d."' OR tag5 = '".$d."'";
-    //  echo $mysqli_query($con,$sql);
+    
      $result =  mysqli_query($con,$sql);
 
      $row = mysqli_fetch_array($result);
@@ -27,10 +25,6 @@ $counter = 0;
      $dankness = intval($row['SUM(upvoteCount)']) + $dankness;
 
      $counter += 1;
-
-
-
-    // echo $d;
    }
 
     echo $dankness;
